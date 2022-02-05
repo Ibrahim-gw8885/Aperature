edc9cee887f3387e402702d94212c5884586d557
Ibrahim-gw8885 <gw8885@wayne.edu>
2022-02-05 18:52:13 -0500

    new

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import tkinter as tk
 
+class Apt:
+    def __init__ (self):
+        pass
+
+
+    class gui:
+    
+        def __init__(self):
+            
+            self.root = tk.Tk()
+            self.frame = tk.Frame(self.root)
+            self.frame.pack()
+
+
+
+            self.root.mainloop()
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+
+
+root.mainloop()
