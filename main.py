5d3f4f825286fee89dfceea21374ad474cc02557
Ibrahim-gw8885 <gw8885@wayne.edu>
2022-02-05 21:07:40 -0500

    reading csv files method made

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
+from asyncore import write
 import tkinter as tk
+import datetime
+import pandas as pd
+from pandas import read_csv
+import csv
 
 shawzub = 1
 
 
-class Apt:
+class apt:
+
+    fridge = 'https://github.com/Ibrahim-gw8885/Aperature/blob/main/fridge.csv'
+
     def __init__ (self):
         pass
+    
+    def returnDaysRemaining():
+        pass
 
+    def readItemFromCsv():
+        with open(apt.fridge, newline='', mode = 'r'):
+            for lines in apt.fridge:
+                print(lines)
+    
+    def writeItemToCsv():
+        with open(apt.fridge, mode = 'r'):
+            for lines in apt.fridge:
+                print(lines)
 
     class gui:
-    
         def __init__(self):
-            
             pass
         
         def inputWidget():
             inputWidgetCol = tk.Frame(root)
 
-root = tk.Tk()
-frame = tk.Frame(root)
-frame.pack()
-
-
-
-root.mainloop()
+apt.writeItemToCsv()
