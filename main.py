4eadf3a73c8d50b1068682b340b13104be6e9a6b
Shawzub Raza <79495823+theshazaminator@users.noreply.github.com>
2022-02-06 05:46:14 -0500

    added years

--- main.py
+++ main.py
@@ -65,16 +65,28 @@ class apt:
 
 
     def determineExpired():
+        
         #pulling the information from the fridge file
-        with open('fridge2.0.csv') as fridge_csv:
+        with open('fridge.csv') as fridge0_csv:
+  
+            fridge0_reader = csv.DictReader(fridge0_csv, delimiter=',')
+            
+            #putting the information read in fridge for a list of years
+            year = [int(book0['expyear']) for book0 in fridge0_reader]
+
+        
+        
+        #pulling the information from the fridge file
+        with open('fridge.csv') as fridge_csv:
   
             fridge_reader = csv.DictReader(fridge_csv, delimiter=',')
+            
             #putting the information read in fridge for a list of months
             month = [int(book['expmonth']) for book in fridge_reader]
 
 
         #pulling the information from the fridge file
-        with open('fridge2.0.csv') as fridge1_csv:
+        with open('fridge.csv') as fridge1_csv:
             fridge1_reader = csv.DictReader(fridge1_csv, delimiter=',')
 
 
@@ -83,7 +95,7 @@ class apt:
 
 
         #pulling the information from the fridge file
-        with open('fridge2.0.csv') as fridge1_csv:
+        with open('fridge.csv') as fridge1_csv:
             fridge1_reader2 = csv.DictReader(fridge1_csv, delimiter=',')
 
 
@@ -94,10 +106,10 @@ class apt:
         current_time = date.today()
 
         #A loop fuction which will take each value from the list to seprate them into day and month 
-
         for num in range(len(day)):
+            
             #this will format the information we have to make a easier to compare to the current date
-            exp_date = date(2022, month[num], day[num])
+            exp_date = date(year[num], month[num], day[num])
             new = products[num]
     
         #this will out put the products that have expired because the products that havent exipred will not show up
